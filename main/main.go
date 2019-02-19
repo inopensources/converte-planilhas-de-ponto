7e1f7494256972df7db23b368463001406a6a2d3
fernandesleticia <leticiafernandes777@gmail.com>
2019-02-19 15:08:59 -0300

    teste com teagle e extrame

--- main/main.go
+++ main/main.go
@@ -1,9 +1,11 @@
 package main
 
-
 import (
-	"../src/github/360EntSecGroup-Skylar/excelize"
+
+	"../../github.com/360EntSecGroup-Skylar/excelize"
+	"../src/github/tealeg/xlsx"
 	"fmt"
+
 )
 
 
@@ -45,14 +47,52 @@ func getCell(sheet string ,axis string, pathToFile string) string {
 
 
 func main() {
+
 	print("=======================\n"+
 		         "Conversor Licença XSLX\n" +
 		         "========================\n")
 
-	//getting a single cell
+	/*//getting a single cell
 	getCell("Sheet1", "B1", "../samples/file_example_XLSX_10.xlsx")
 
 	//getting all rows
-	getAllRows("Sheet1", "../samples/file_example_XLSX_10.xlsx")
+	getAllRows("Sheet1", "../samples/file_example_XLSX_10.xlsx")*/
+
+
+	//getting a single cell
+	//getCell("Sheet1", "B1", "../samples/file_example_XLSX_10.xlsx")
+	//getting all rows
+	//getAllRows("Sheet1", "../samples/file_example_XLS_10.xls")
+
+	//using extrame library
+/*	if xlFile, err := Open("Table.xls", "utf-8"); err == nil {
+		if sheet1 := xlFile.GetSheet(0); sheet1 != nil {
+			fmt.Print("Total Lines ", sheet1.MaxRow, sheet1.Name)
+			col1 := sheet1.Rows[0].Cols[0]
+			col2 := sheet1.Rows[0].Cols[0]
+			for i := 0; i <= (int(sheet1.MaxRow)); i++ {
+				row1 := sheet1.Rows[uint16(i)]
+				col1 = row1.Cols[0]
+				col2 = row1.Cols[11]
+				fmt.Print("\n", col1.String(xlFile), ",", col2.String(xlFile))
+			}
+		}
+	}*/
+
+	//using tealeg library
+	excelFileName := "../samples/file_example_XLS_10.xls"
+	xlFile, err := xlsx.OpenFile(excelFileName)
+	if err != nil {
+		fmt.Print(err)
+	}
+	for _, sheet := range xlFile.Sheets {
+		for _, row := range sheet.Rows {
+			for _, cell := range row.Cells {
+				text := cell.String()
+				fmt.Printf("%s\n", text)
+			}
+		}
+	}
+
 
 }
